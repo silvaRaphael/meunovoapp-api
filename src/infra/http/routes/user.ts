bcef4dfc198eb01467bd6f45462b7804af45788c
silvaRaphael <raphaeltiago02@gmail.com>
2023-12-20 21:28:05 -0300

    fix: users middlewares

--- src/infra/http/routes/user.ts
+++ src/infra/http/routes/user.ts
@@ -48,8 +48,7 @@ routes.put("/:id?", AuthMiddleware, (req, res) => {
 	userController.updateUser(req, res);
 });
 
-// routes.get("/", AuthMiddleware, RoleMiddleware, (req, res) => {
-routes.get("/", (req, res) => {
+routes.get("/", AuthMiddleware, RoleMiddleware, (req, res) => {
 	userController.getAllUsers(req, res);
 });
 
@@ -57,8 +56,7 @@ routes.get("/profile", AuthMiddleware, (req, res) => {
 	userController.getProfile(req, res);
 });
 
-// routes.get("/:id", AuthMiddleware, RoleMiddleware, (req, res) => {
-routes.get("/:id", (req, res) => {
+routes.get("/:id", AuthMiddleware, RoleMiddleware, (req, res) => {
 	userController.getUser(req, res);
 });
 
