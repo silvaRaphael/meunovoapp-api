9204c996730b2ee1ce2197aee94edd01305110ab
silvaRaphael <raphaeltiago02@gmail.com>
2024-11-04 15:28:49 -0300

    add: conversor zpl

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import { createServer } from "node:http";
 import { routes } from "./routes";
 import files from "./routes/file";
 import { httpAllowedOrigins } from "../config/allowed-origins";
+import { zplToPdfConversor } from "./routes/zpl-to-pdf-conversor";
 
 const app = express();
 const server = createServer(app);
@@ -23,6 +25,7 @@ app.use(express.static("/app/files"));
 
 app.use("/v1", routes);
 app.use("/files", files);
+app.use("/conversor-zpl", zplToPdfConversor);
 
 app.get("/", (_, res) => res.end());
 app.get("*", (_, res) => res.status(404).end());
